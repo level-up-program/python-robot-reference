cda40d5ed14f6285d75529de8218b3bc2befb9f7
JP White <jpwhite3@users.noreply.github.com>
2023-03-23 12:28:03 -0400

    Update MoveLibrary.py

--- tests/robot/MoveLibrary.py
+++ tests/robot/MoveLibrary.py
@@ -13,9 +13,12 @@ class MoveLibrary:
     def move_in_direction(self, direction):
         self.controller = GameController()
         self.controller.set_character_position((self.start_x, self.start_y))
-
+        self.controller.status.move_count = self.move_count
         self.controller.move(Direction[direction])
 
+    def initialize_character_moveCount_with(self, move_count):
+        self.move_count = move_count
+        
     def character_xposition_should_be(self, expected):
         end_x = self.controller.status.current_position[0]
         if end_x != expected:
@@ -28,4 +31,11 @@ class MoveLibrary:
         if end_y != expected:
             raise AssertionError(
                 "%s != %s" % (end_y, expected)
+            )
+
+    def character_moveCount_should_be(self, expected):
+        move_count = self.controller.status.move_count
+        if move_count != expected:
+            raise AssertionError(
+                "%s != %s" % (move_count, expected)
             )
