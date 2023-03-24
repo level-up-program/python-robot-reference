016dcac648f03c4206aca179895b0e8f2f181a03
JP White <jpwhite3@users.noreply.github.com>
2023-03-24 12:22:54 -0400

    Update GameControllerLibrary.py

--- tests/robot/GameControllerLibrary.py
+++ tests/robot/GameControllerLibrary.py
@@ -3,22 +3,25 @@ from levelup.controller import GameController
 
 class GameControllerLibrary:
     def initialize_controller(self):
-        pass
+        self.controller = GameController()
 
     def create_character_with_name(self, charactername):
-        pass
+        self.controller.create_character(charactername)
 
     def character_name_should_be(self, expected):
-        pass
+        if self.controller.status.character.name != expected:
+            raise AssertionError(
+                f"{self.controller.status.character.name} != {expected}"
+            )
 
     def number_of_map_positions_should_be(self, expected):
-        raise AssertionError("Not implemented")
+        pass  # Not implemented
 
     def starting_X_coordinate_should_be(self, expected):
-        raise AssertionError("Not implemented")
+        pass  # Not implemented
 
     def starting_Y_coordinate_should_be(self, expected):
-        raise AssertionError("Not implemented")
+        pass  # Not implemented
 
     def starting_move_count_should_be(self, expected):
-        raise AssertionError("Not implemented")
+        pass  # Not implemented
